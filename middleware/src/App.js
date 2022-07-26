832b751e5a163e9335237ec7eca288586c5ce32d
S-MunSoo <godmsoo93@gmail.com>
2022-07-26 17:17:54 +0900

    redux toolkit 장바구니 학습

--- middleware/src/App.js
+++ middleware/src/App.js
@@ -1,15 +1,71 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+// import { useEffect } from "react";
+// import { uiAction } from "./store/ui-slice";
+
+// import Notification from "./components/UI/Notification";
+
+// let isInitial = true;
 
 function App() {
+  // const dispatch = useDispatch();
   const showCart = useSelector((state) => state.uiCard.cartIsVisible);
+  // const cart = useSelector((state) => state.cart);
+  // const notification = useSelector((state) => state.ui.notification);
+
+  // useEffect(() => {
+  //   const sendCartDate = async () => {
+  //     dispatch(
+  //       uiAction.showNotification({
+  //         status: "pending",
+  //         title: "Sending...",
+  //         message: "Sending cart data!",
+  //       })
+  //     );
+  //     const res = await fetch(
+  //       "https://react-http-6b4a6.firebaseio.com/cart.json",
+  //       {
+  //         method: "PUT",
+  //         body: JSON.stringify(cart),
+  //       }
+  //     );
+  //     if (!res.ok) {
+  //       throw new Error("Sending cart data failed");
+  //     }
+  //     dispatch(
+  //       uiAction.showNotification({
+  //         status: "success",
+  //         title: "Success!",
+  //         message: "Sent cart data successfully",
+  //       })
+  //     );
+  //   };
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+  //   sendCartDate().catch((error) => {
+  //     dispatch(
+  //       uiAction.showNotification({
+  //         status: "error",
+  //         title: "Error!",
+  //         message: "Sending cart failed",
+  //       })
+  //     );
+  //   });
+  // }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- middleware/src/store/ui-slice.js
+++ middleware/src/store/ui-slice.js
@@ -4,10 +4,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false }, // 장바구니가 표시 여부 확인
+  initialState: { cartIsVisible: false, notification: null }, // 장바구니가 표시 여부 확인
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
